787dbdc88a92bf61ccbb505157aa12f105c9388a
david <david@pixkie.co.uk>
2022-04-20 13:54:25 +0100

    screens and the screen navigations set up

---
+++ RootNavigator.js
@@ -0,0 +1,107 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+import {Ionicons} from "@expo/vector-icons";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createStackNavigator} from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import BrowseScreen from "./screens/BrowseScreen";
+import GroceryScreen from "./screens/GroceryScreen";
+import OrdersScreen from "./screens/OrdersScreen";
+import AccountScreen from "./screens/AccountScreen";
+
+const BottomTab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+
+const HomeScreenStack = () => {
+	return (
+		<HomeStack.Navigator screenOptions ={() => ({
+			headerShown: false,
+		})}>
+			<HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+		</HomeStack.Navigator>
+	);
+};
+const RootNavigator = () => {
+	return (
+		<BottomTab.Navigator
+			initialRouteName={HomeScreen}
+			screenOptions={({route}) => ({
+				tabBarActiveTintColor: "black",
+				tabBarInactiveTintColor: "grey",
+				showLabel: true,
+			})}
+		>
+			<BottomTab.Screen
+				name="HomeScreenStack"
+				component={HomeScreenStack}
+				options={{
+					tabBarIcon: ({color, focused}) => (
+						<Ionicons
+							name={focused ? "home" : "home-outline"}
+							size={24}
+							color="black"
+						/>
+					),
+					headerShown: false,
+				}}
+			/>
+			<BottomTab.Screen
+				name="BrowseScreen"
+				component={BrowseScreen}
+				options={{
+					tabBarIcon: ({color, focused}) => (
+						<Ionicons
+							name={focused ? "search" : "search-outline"}
+							size={24}
+							color="black"
+						/>
+					),
+					headerShown: false,
+				}}
+			/>
+			<BottomTab.Screen
+				name="Grocery"
+				component={GroceryScreen}
+				options={{
+					tabBarIcon: ({color, focused}) => (
+						<Ionicons
+							name={focused ? "basket" : "basket-outline"}
+							size={24}
+						/>
+					),
+					headerShown: false,
+				}}
+			/>
+			<BottomTab.Screen
+				name="Order"
+				component={OrdersScreen}
+				options={{
+					tabBarIcon: ({color, focused}) => (
+						<Ionicons
+							name={focused ? "cart" : "cart-outline"}
+							size={24}
+						/>
+					),
+					headerShown: false,
+				}}
+			/>
+			<BottomTab.Screen
+				name="Account"
+				component={AccountScreen}
+				options={{
+					tabBarIcon: ({color, focused}) => (
+						<Ionicons
+							name={focused ? "person" : "person-outline"}
+							size={24}
+						/>
+					),
+					headerShown: false,
+				}}
+			/>
+		</BottomTab.Navigator>
+	);
+};
+
+export default RootNavigator;
+
+const styles = StyleSheet.create({});
